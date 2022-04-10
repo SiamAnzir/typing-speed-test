6462dacd00c5762d7fd3ce446057a96624b2b1cc
SiamAnzir <siamanzir18@gmail.com>
2022-04-11 01:49:51 +0600

    Set game loop

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import './App.css';
-import {useState,useEffect} from "react";
+import {useState,useEffect,useRef} from "react";
 import randomWords from "random-words";
-//import randomSentence from "random-sentence";
-
 
 const App = () => {
+  const time = 10;
+
   const [words,setWords] = useState([]);
-  const [count,setCount] = useState(10);
+  const [count,setCount] = useState(time);
   const [currentValue, setCurrentValue] = useState("");
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [correctWord, setCorrectWord] = useState(0)
   const [incorrectWord, setIncorrectWord] = useState(0)
+  const [status,setStatus] = useState("watching");
+  const textInput = useRef(null);
 
   useEffect(() =>{
     setWords(createWords);
   },[]);
 
+  useEffect(() => {
+    if(status === "started"){
+      textInput.current.focus()
+    }
+  },[status]);
+
   const createWords = () => {
     return new Array(250).fill(null).map(() => randomWords())
   }
   const countDownStart = () => {
-    let interval = setInterval(() => {
-      setCount((prevCount) => {
-        if(prevCount === 0){
-          clearInterval(interval);
-        }
-        else{
-          return prevCount - 1;
-        }
-      })
-    },1000)
+    if(status === "finished"){
+      setWords(createWords());
+      setCurrentWordIndex(0);
+      setCorrectWord(0);
+      setIncorrectWord(0);
+    }
+    if(status !== "started"){
+      setStatus("started");
+      let interval = setInterval(() => {
+        setCount((prevCount) => {
+          if(prevCount === 0){
+            clearInterval(interval);
+            setStatus("finished");
+            setCurrentValue("")
+            return time;
+          }
+          else{
+            return prevCount - 1;
+          }
+        })
+      },1000)
+    }
   }
   const handleKeyUpdate = ({keyCode}) => {
     if(keyCode === 32){
@@ -41,11 +61,9 @@ const App = () => {
   const checkWord = () => {
     if(words[currentWordIndex] === currentValue.trim()){
       setCorrectWord(correctWord + 1);
-      console.log(correctWord);
     }
     else{
       setIncorrectWord(incorrectWord + 1);
-      console.log(correctWord);
     }
   }
   //const words = randomWords(100).join(' ');
@@ -56,32 +74,52 @@ const App = () => {
         <h3>{count}</h3>
       </div>
       <div>
-        <input type="text" onKeyDown={handleKeyUpdate} value={currentValue} onChange={(e) => setCurrentValue(e.target.value)}/>
+        <input ref={textInput} disabled={status !== "started"} type="text" onKeyDown={handleKeyUpdate} value={currentValue} onChange={(e) => setCurrentValue(e.target.value)}/>
       </div>
       <div>
         <button className="primary" onClick={countDownStart}>
-          Start
+          {
+            (status === "finished") ? (
+                "Restart"
+            ) : (
+                "Start"
+            )
+          }
         </button>
       </div>
       <div>
-        {words.map((word,index) => (
-            <span key={index}>
-                <span>
-                  {word.split("").map((char,i) => (
-                      <span key={i}>{char}</span>
-                  ))}
-                </span>
-                <span> </span>
-              </span>
-        ))}
-      </div>
-      <div>
-        <h4>Correct Words</h4>
-        <h5>{correctWord}</h5>
-      </div>
-      <div>
-        <h4>Accuracy</h4>
-        <h5>{Math.round((correctWord / (correctWord + incorrectWord)) * 100)}%</h5>
+        <div>
+          {words.map((word,index) => (
+              <span key={index}>
+                        <span>
+                          {word.split("").map((char,i) => (
+                              <span key={i}>{char}</span>
+                          ))}
+                        </span>
+                        <span> </span>
+                      </span>
+          ))}
+        </div>
+        <div>
+          <div>
+            <h4>Correct Words</h4>
+            <h5>{correctWord}</h5>
+          </div>
+          <div>
+            <h4>InCorrect Words</h4>
+            <h5>{incorrectWord}</h5>
+          </div>
+          <div>
+            <h4>Accuracy</h4>
+            <h5>
+              {(correctWord !== 0 ? (
+                  Math.round((correctWord / (correctWord + incorrectWord)) * 100) + "%"
+              ) : (
+                  "0%"
+              ))}
+            </h5>
+          </div>
+        </div>
       </div>
     </div>
   );
