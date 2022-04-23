b63a0b406d555bc0843f2f7d8dc4d4695f38b961
SiamAnzir <siamanzir18@gmail.com>
2022-04-23 16:43:05 +0600

    [created] navbar-component

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+const navbar = () => {
+    return(
+        <ul className="navbar-class">
+            <li className="project-name">Typing Speed Test</li>
+            <li className="profile-name">Profile Name</li>
+        </ul>
+    )
+}
+
+export default navbar;
