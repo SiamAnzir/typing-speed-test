2a36937ecea781c08112ec271fc3d60543b93f4d
SiamAnzir <siamanzir18@gmail.com>
2022-04-30 01:46:05 +0600

    [Modified] profile-name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-const navbar = () => {
+const navbar = (props) => {
     return(
         <ul className="navbar-class">
             <li className="project-name">Typing Speed Test</li>
-            <li className="profile-name">Profile Name</li>
+            <li className="profile-name">{(props.profileName === "") ? ("Profile Name") : (props.profileName) }</li>
         </ul>
     )
 }
