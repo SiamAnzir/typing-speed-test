4ac67d8686c69b13f9607c70e015d8ab2fc8c2da
SiamAnzir <siamanzir18@gmail.com>
2022-04-30 01:45:44 +0600

    [Modified] profile-name

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
+const Modal = ({ isShowing, modalHandleSubmit,modalInputChange }) => isShowing ? ReactDOM.createPortal(
     <React.Fragment>
         <div className="modal-overlay"/>
         <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
             <div className="modal">
-                <div className="modal-header">
-                    <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <p>
-                    Hello, I'm a modal.
-                </p>
+                <form onSubmit={modalHandleSubmit}>
+                    <label>Type Your Full Name</label>
+                    <input className="modal-input-field" required type="text" placeholder="Profile Name...." minLength="8" onChange={modalInputChange}/>
+                    <button className="modal-button" type="submit">Submit</button>
+                </form>
             </div>
         </div>
     </React.Fragment>, document.body
 ) : null;
 
+
 export default Modal;
+
+/**
+ * <div className="modal-header">
+ *                     <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+ *                         <span aria-hidden="true">&times;</span>
+ *                     </button>
+ *                 </div>**/
