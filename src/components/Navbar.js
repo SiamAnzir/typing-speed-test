a9f55e42ffd20cbf2b3611f69f11d3b6efe04c2e
SiamAnzir <siamanzir18@gmail.com>
2022-05-04 19:24:54 +0600

    [Added] profile icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
+import profilePic from '../img/profile.png';
+
 const navbar = (props) => {
     return(
         <ul className="navbar-class">
             <li className="project-name">Typing Speed Test</li>
-            <li className="profile-name">{(props.profileName === "") ? ("Profile Name") : (props.profileName) }</li>
+            <li className="profile-name"> <img src={profilePic} alt="profilePic" style={{height:'30px', width:'40px'}}/> {(props.profileName === "") ? ("Profile Name") : (props.profileName) }</li>
         </ul>
     )
 }
