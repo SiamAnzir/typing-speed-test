28c5113ece5b10b92adb79955afd326cb231becf
SiamAnzir <siamanzir18@gmail.com>
2022-05-23 20:45:15 +0600

    Updates

---
+++ precache-manifest.bf0eeed6fc315ecd2855664f8a1c009c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c53da4597c980f00968",
+    "url": "/typing-speed-test/static/css/main.f473621e.chunk.css"
+  },
+  {
+    "revision": "9c53da4597c980f00968",
+    "url": "/typing-speed-test/static/js/main.9c53da45.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/typing-speed-test/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "226818c6c2166b17af17",
+    "url": "/typing-speed-test/static/js/2.226818c6.chunk.js"
+  },
+  {
+    "revision": "e11d7f08694afe07eb33",
+    "url": "/typing-speed-test/static/js/runtime~main.e11d7f08.js"
+  },
+  {
+    "revision": "d79e75b7c6c2e2e37523bf563e3b7029",
+    "url": "/typing-speed-test/static/media/profilepic.d79e75b7.png"
+  },
+  {
+    "revision": "2eb74497f6562b360d3c3da615180f7a",
+    "url": "/typing-speed-test/index.html"
+  }
+];
